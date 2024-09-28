0ca56aebdf6805040b29dea2d0f088b93326da91
liujia <liujia0905@gmail.com>
2024-09-28 11:29:24 +0800

    Init FragmentBestPractice

---
+++ chapter05/FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsContentActivity.kt
@@ -0,0 +1,20 @@
+package com.example.fragmentbestpractice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class NewsContentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_news_content)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
